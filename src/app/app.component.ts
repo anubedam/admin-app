025ea60a9fa4deb97f6518b7362085f111bf4024
Antonio Úbeda Montero <anubedam@gmail.com>
2023-10-10 20:20:47 +0200

    add aside and main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuOpcion } from './interfaces/menu-opcion.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'admin-app';
+export class AppComponent implements OnInit {
+  MenuOpciones: MenuOpcion[] = [];
+
+  ngOnInit(): void {
+    this.MenuOpciones = [
+      { texto: 'Empresas', url: '#', icono: '' },
+      { texto: 'Actividades', url: '#', icono: '' },
+    ];
+  }
 }
---
+++ src/app/interfaces/menu-opcion.interface.ts
@@ -0,0 +1,5 @@
+export interface MenuOpcion {
+  texto: string;
+  url: string;
+  icono: string;
+}
